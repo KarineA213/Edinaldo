0c4dd34b36f7d4dad199c0462a027fa514cbc95a
karine <karineaguiar.job@gmail.com>
2026-07-17 03:23:55 -0300

    primeiro commit

---
+++ docs/assets/javascript/carrossel.js
@@ -0,0 +1,30 @@
+
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal', 
+    loop: true,
+    
+    spaceBetween: 24, 
+    
+    slidesPerView: 1, 
+    
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3, 
+        spaceBetween: 30,
+      },
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
